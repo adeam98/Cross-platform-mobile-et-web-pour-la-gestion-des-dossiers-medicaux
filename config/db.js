c112069f53ad019aa19b03db54b12f3542a8edf8
Bounoualailyas <156212879+Bounoualailyas@users.noreply.github.com>
2025-04-24 16:23:36 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-require('dotenv').config();
+require('dotenv').config();/*Cette ligne charge les variables d’environnement définies dans le fichier .env et les rend accessibles via process.env*/
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -7,7 +7,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-});
+});/* ici on crée une instance de connexion à PgSL et on a mis dadans la config nécessaire pour etablir la connexion et ca grace à dotenv qui est en haut  */
 
 pool.connect()
   .then(() => console.log('✅ PostgreSQL connected'))
