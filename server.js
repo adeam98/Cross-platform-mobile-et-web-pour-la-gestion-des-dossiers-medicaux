9a039659cbdd5f7e6fc6017707aaa4625298df6e
adeam98 <Adamtraibi952@gmail.com>
2025-06-30 20:15:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const path = require('path');  
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  next();
+});
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors());
+
 app.use(bodyParser.json());
 
 const authRoutes = require('./routes/create_account');
@@ -27,11 +35,12 @@ const consultationRoutes = require('./routes/patient/consultation');
 app.use('/api/patient', consultationRoutes);
 
 const analyseRoutes = require('./routes/patient/analyse');
-app.use('/api/patient', analyseRoutes); 
+app.use('/api/patient', analyseRoutes);
 
 const rechercher = require('./routes/medcin/rechercher');
 app.use('/api/medcin', rechercher); 
-app.use('/api/centre',rechercher);
+app.use('/api/centre', rechercher);
+
 const medcinRoutes = require('./routes/medcin/maladie');
 app.use('/api/medcin', medcinRoutes);
 
@@ -40,15 +49,17 @@ app.use('/api/medcin', rdvmedin);
 
 const medciament = require('./routes/medcin/medicament');
 app.use('/api/medcin', medciament);
+
 const consultation = require('./routes/medcin/consultation');
 app.use('/api/medcin', consultation);
-
 
 const analyse = require('./routes/medcin/analyse');
 app.use('/api/medcin', analyse);
 
-const analysecentre= require('./routes/centre_analyse/analyse');
-app.use('api/centre',analysecentre);
+const analysecentre = require('./routes/centre_analyse/analyse');
+app.use('/api/centre', analysecentre);
+
+
 app.listen(PORT, () => {
   console.log(`✅ Backend is running on http://localhost:${PORT}`);
 });
