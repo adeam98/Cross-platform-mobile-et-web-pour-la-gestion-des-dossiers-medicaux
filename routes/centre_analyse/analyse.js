6c7ec5fb35e72675a18bf330384ac266b451e69e
adeam98 <Adamtraibi952@gmail.com>
2025-06-30 20:11:15 +0100

    Update analyse.js

--- routes/centre_analyse/analyse.js
+++ routes/centre_analyse/analyse.js
@@ -1,8 +1,22 @@
 const express = require('express');
-const { AddAnalyse} = require('../../controllers/centre_analyse/analyse');
-
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const { AddAnalyse } = require('../../controllers/centre_analyse/analyse');
+const { servePdfFile } = require('../../controllers/filepdf'); 
+// Set up multer for file storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Directory where files will be stored
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname); // Ensure unique file name
+  }
+});
 
-router.post('/add/analyse/:id_user', AddAnalyse);
+const upload = multer({ storage });
 
+
+router.post('/addAnalyse/:id_user', upload.single('result'), AddAnalyse);
+router.get('/uploads/:fileName', servePdfFile);
 module.exports = router;
