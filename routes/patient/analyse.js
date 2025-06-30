290fb03fe1dc76e5840821f8698c501123d71075
adeam98 <Adamtraibi952@gmail.com>
2025-06-30 20:12:38 +0100

    Update analyse.js

--- routes/patient/analyse.js
+++ routes/patient/analyse.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { addAnalyse } = require('../../controllers/medcin/analyse');
 const { getAnalyseByEtat } = require('../../controllers/patient/analyse');
-
-router.get('/analyse/:id_user/:etat', getAnalyseByEtat);
-
+const { servePdfFile} = require('../../controllers/filepdf');
+router.get('/analysep/:id_user/:etat',getAnalyseByEtat)
+router.post('/add/analyse/:id_user', addAnalyse);
+router.get('/analyser/:id_user/:etat',getAnalyseByEtat)
+router.get('/uploads/:fileName', servePdfFile);
 module.exports = router;
