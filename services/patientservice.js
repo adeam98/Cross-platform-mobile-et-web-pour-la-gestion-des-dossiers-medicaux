b6fac410b251069e683c0f26de110b552e9d2b30
adeam98 <Adamtraibi952@gmail.com>
2025-06-30 20:47:24 +0100

    Update patientservice.js

--- services/patientservice.js
+++ services/patientservice.js
@@ -1,20 +1,23 @@
-import axios from 'axios';
+import api from './api';
 
-const PATIENT_API_URL = 'http://192.168.0.29:5000/api/patient';
+const PATIENT_API_URL = 'http://192.168.1.2:5000/api/patient';
 
 export const getALLMaladie = async (id_user) => {
-  return await axios.get(`${PATIENT_API_URL}/maladie/${id_user}`);
+  return await api.get(`${PATIENT_API_URL}/maladie/${id_user}`);
 };
 export const getALLRdv = async (id_user) => {
-  return await axios.get(`${PATIENT_API_URL}/rdv/${id_user}`);
+  return await api.get(`${PATIENT_API_URL}/rdv/${id_user}`);
 };
 export const getAllMedicament = async (id_user) => {
-  return await axios.get(`${PATIENT_API_URL}/medicament/${id_user}`);
+  return await api.get(`${PATIENT_API_URL}/medicament/${id_user}`);
 };
 
 export const getALLconsultation = async (id_user) => {
-  return await axios.get(`${PATIENT_API_URL}/consultation/${id_user}`);
+  return await api.get(`${PATIENT_API_URL}/consultation/${id_user}`);
 };
 export const getAnalyse = async (id_user, etat) => {
-  return await axios.get(`${PATIENT_API_URL}/analyse/${id_user}/${etat}`);
+  return await api.get(`${PATIENT_API_URL}/analyse/${id_user}/${etat}`);
+};
+export const getPdfUrl = (filename) => {
+  return `http://192.168.1.8:5000/api/patient/uploads/${filename.replace('/uploads/', '')}`;
 };
